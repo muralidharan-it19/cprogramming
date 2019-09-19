e392ee8a8d691a8cdd16487748f6769264bf2a1e
muralidharan-it19 <muralidharan.it19@bitsathy.ac.in>
2019-09-19 11:20:54 +0530

    tenth commit

---
+++ smallest.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d;
+printf("enter values for a,b,c,d:");
+scanf("%d %d %d %d",&a,&b,&c,&d);
+if(a<b&&a<c)
+{
+printf("%d is smaller",a);
+}
+else if(b<c&&b<d)
+{
+printf("%d is smaller",b);
+}
+else if(c<d)
+{
+printf("%d is smaller",c);
+}
+else 
+{ 
+printf("%d is smaller",d);
+}
+}
