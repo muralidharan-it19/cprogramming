464204f6f88731603d81c22ec406e66185ea1645
muralidharan-it19 <muralidharan.it19@bitsathy.ac.in>
2019-10-03 11:17:05 +0530

    commit 16

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i;
+printf("Enter value for array:");
+for(i=0;i<=10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=10;i++)
+{
+printf("\n%d",a[i]);
+}
+}
+
