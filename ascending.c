91f499f51bed49a8d8ed423710badbc001c1680b
muralidharan-it19 <muralidharan.it19@bitsathy.ac.in>
2019-10-10 11:16:46 +0530

    commit 20

---
+++ ascending.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main() 
+{
+    int a[10],i,j,c;
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<5;j++)
+        {
+            if(a[i]>a[j])
+            {
+                c=a[i];
+                a[i]=a[j];
+                a[j]=c;
+            }
+        }
+    }
+     printf("\narray is sorted in descending order:\n");
+    for(i=0;i<5; i++)
+    {
+        printf("%d ", a[i]);
+    }
+	        printf("\n\n");
+}
